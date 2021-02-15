ef3471cb808a42edd076b9e52bb3d4365cc47ac0
Philip Taylor <phil@zea.live>
2021-02-15 11:16:11 -0500

    feat: added simple file parser example code to the CustomTreeItem

--- src/MyCustomTreeItem.js
+++ src/MyCustomTreeItem.js
@@ -1,4 +1,4 @@
-import { Box3, Color, Vec3Parameter, ColorParameter, TreeItem, Vec3 } from '@zeainc/zea-engine'
+import { Box3, Color, Vec3Parameter, ColorParameter, TreeItem, Vec3, resourceLoader } from '@zeainc/zea-engine'
 
 /**
  * Base class that represents geometry items with layering, overlaying and cut away features.
@@ -31,6 +31,54 @@ class MyCustomTreeItem extends TreeItem {
     const localBBox = new Box3(size.scale(-0.5), size.scale(0.5))
     bbox.addBox3(localBBox, this.getParameter('GlobalXfo').getValue().toMat4())
     return bbox
+  }
+
+  /**
+   * The loadDataFile method.
+   * @param {string} url - The bounding box value.
+   * @return {Promise} - the promise that resolves once the file is loaded
+   * @private
+   */
+  loadDataFile(url) {
+    return new Promise((resolve, reject) => {
+      resourceLoader.incrementWorkload(1)
+      resourceLoader.loadFile('text', url).then(
+        (txt) => {
+          parseMtlData(fileData)
+          resourceLoader.incrementWorkDone(1)
+          resolve()
+        },
+        function (error) {
+          reject(error)
+        }
+      )
+    })
+  }
+
+  /**
+   * Parses the contents of the loaded file
+   * @param {string} fileData - The contents of the ASCII file we are loading.
+   */
+  parseData(fileData) {
+    const lines = fileData.split('\n')
+    const WHITESPACE_RE = /\s+/
+
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i].trim()
+
+      // Skip over comment lines
+      if (line.startsWith('#')) continue
+
+      // for each line, split it into elements.
+      const elements = line.split(WHITESPACE_RE)
+      const key = elements.shift()
+      const value = elements.join(' ')
+      switch (key) {
+        default:
+          console.log('Unhandled line', line)
+        // console.warn("Unhandled material parameter: '" + key +"' in:" + filePath);
+      }
+    }
   }
 }
 
